21a29ecd4cc84a83925230332a2219b4ba1c5235
Felix Ogucha <fenicfelix@gmail.com>
2025-08-23 12:49:52 +0100

    Updates ncba config

--- config/ncba.php
+++ config/ncba.php
@@ -1,14 +1,26 @@
 <?php
-
+$environment = env('NCBA_ENV', 'sandbox');
+$env_key = strtoupper($environment);
 return [
-    'env' => env('NCBA_ENV', 'sandbox'), // sandbox or production
+    'env' => $environment, // sandbox or production
     'debug' => env('NCBA_DEBUG', true),
-    'sandbox' => [
-        'api_key' => env('NCBA_SANDBOX_API_KEY', 'T3st123'),
-        'url' => 'https://devuat.ncbagroup.com/api',
-    ],
-    'production' => [
-        'api_key' => env('NCBA_API_KEY', 'T3st123'),
-        'url' => 'https://ncbagroup.com/api',
-    ],
+    'api_key' => env('NCBA_' . $env_key . '_API_KEY', 'T3st123'),
+    'username' => env('NCBA_' . $env_key . '_USERNAME', 'sandbox_user'),
+    'password' => env('NCBA_' . $env_key . '_PASSWORD', 'sandbox_password'),
+    'url' => env('NCBA_' . $env_key . '_URL', 'https://apidev.ncbagroup.com/api/v1'),
 ];
+
+/*
+ * ENVIRONMENT VARIABLES
+ *
+ * NCBA_ENV
+ * NCBA_DEBUG
+ * NCBA_SANDBOX_API_KEY
+ * NCBA_SANDBOX_USERNAME
+ * NCBA_SANDBOX_PASSWORD
+ * NCBA_SANDBOX_URL
+ * NCBA_PRODUCTION_API_KEY
+ * NCBA_PRODUCTION_USERNAME
+ * NCBA_PRODUCTION_PASSWORD
+ * NCBA_PRODUCTION_URL
+ */
